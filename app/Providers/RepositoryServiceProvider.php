a5fafb9785ab7dca524919c46a2a0248afbcad5f
Juan D Carrillo <delnortemx@gmail.com>
2019-08-23 16:49:20 -0600

    feat: enable Spdb entity
     - model
     - controller
     - requests
     - repositories
     - migration
     - register in service provider

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -35,6 +35,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(\App\Repositories\AnalysisRepository::class, \App\Repositories\AnalysisRepositoryEloquent::class);
         $this->app->bind(\App\Repositories\SmdRepository::class, \App\Repositories\SmdRepositoryEloquent::class);
         $this->app->bind(\App\Repositories\SaRepository::class, \App\Repositories\SaRepositoryEloquent::class);
+        $this->app->bind(\App\Repositories\SpdbRepository::class, \App\Repositories\SpdbRepositoryEloquent::class);
         //:end-bindings:
     }
 }
